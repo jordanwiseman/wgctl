d0ad1bb9be831adf89be4e304d9d44ca93910e29
Jordan Wiseman <jordanwiseman@msn.com>
2026-04-07 13:00:49 +0000

    changed jq syntax in apply

--- wgctl.sh
+++ wgctl.sh
@@ -460,13 +460,8 @@ apply_interface() {
         ' <<< "$json_config"
         # Build the peers section (one block per peer, with an empty line preceding each)
         jq -r '.peers | to_entries[] | select(.value.status == "enable")
-            | "\n[Peer]\nPublicKey = \(.value.publicKey)\nAllowedIPs = \(.value.allowedIPs)"' <<< "$json_config"
-
-        # add a PSK if one exists for the peer
-        local usePSK= $(jq -r --arg peer "$peer_name" '.peers[$peer].usePSK' <<< "$json_config")
-        if [ ! -z  $usePSK ]; then
-            "\nPresharedKey = $usePSK"
-        fi
+            | "\n[Peer]\nPublicKey = \(.value.publicKey)\nAllowedIPs = \(.value.allowedIPs)"
+            + (.value.usePSK? // empty | "\nPresharedKey = \(.)"' <<< "$json_config"
     )
 
     # Save WireGuard configuration to file
