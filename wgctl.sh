ba1010e0199219d411cef6e6ef9f121cf2f043ab
Jordan Wiseman <jordanwiseman@msn.com>
2026-04-07 12:45:02 +0000

    syntax, again

--- wgctl.sh
+++ wgctl.sh
@@ -463,8 +463,7 @@ apply_interface() {
             | "\n[Peer]\nPublicKey = \(.value.publicKey)\nAllowedIPs = \(.value.allowedIPs)"' <<< "$json_config"
 
         # add a PSK if one exists for the peer
-        local usePSK 
-        usePSK = jq -r --arg peer "$peer_name" '.peers[$peer].usePSK' <<< "$json_config"
+        local usePSK= $(jq -r --arg peer "$peer_name" '.peers[$peer].usePSK' <<< "$json_config")
         if [ ! -z  $usePSK ]; then
             "\nPresharedKey = $usePSK"
         fi
